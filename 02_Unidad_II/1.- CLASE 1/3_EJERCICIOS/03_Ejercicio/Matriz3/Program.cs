b9b2010c842e22294bddd991f3fd48246b202faa
luisguillermomolero <luisguillermomolero@gmail.com>
2021-09-24 09:39:10 -0500

    Third commit

--- 02_Unidad_II/1.- CLASE 1/3_EJERCICIOS/03_Ejercicio/Matriz3/Program.cs
+++ 02_Unidad_II/1.- CLASE 1/3_EJERCICIOS/03_Ejercicio/Matriz3/Program.cs
@@ -6,15 +6,15 @@ namespace AplicacionBase
 	{
 		static void Main(string[] args)
 		{
-			int FILAS_MATRIZ = 2;
-			int COLUMNAS_MATRIZ = 2;
-			int[,] Matriz = new int[FILAS_MATRIZ+1, COLUMNAS_MATRIZ+1];
+			int FILAS_MATRIZ = 3;
+			int COLUMNAS_MATRIZ = 3;
+			int[,] Matriz = new int[FILAS_MATRIZ, COLUMNAS_MATRIZ];
 			int[] diagonalVector = new int[FILAS_MATRIZ+1];
 			
-			// Introduccion de numeros para rellenar la matriz 5*5
+			// Introduccion de numeros para cargar la matriz 5*5
 
-			for (int f = 0; f <= FILAS_MATRIZ; f++){
-				for (int c = 0; c <= COLUMNAS_MATRIZ; c++){
+			for (int f = 0; f < FILAS_MATRIZ; f++){
+				for (int c = 0; c < COLUMNAS_MATRIZ; c++){
 					Console.Write($"Digite un numero para la posicion {f},{c} : " );
 					Matriz[f,c] = int.Parse(System.Console.ReadLine());
 					// Diagonal del vector
@@ -24,15 +24,15 @@ namespace AplicacionBase
 			}
 			// Imprimir matriz//
 			Console.WriteLine("\nLa matriz resultado es : ");
-			for (int c = 0; c <= COLUMNAS_MATRIZ; c++){
-				for (int f = 0; f <= FILAS_MATRIZ; f++){
+			for (int c = 0; c < COLUMNAS_MATRIZ; c++){
+				for (int f = 0; f < FILAS_MATRIZ; f++){
 					Console.Write(Matriz[f,c] + " ");
 				}
 				Console.WriteLine(" ");		
 			}
 			Console.WriteLine("");
 			Console.WriteLine("La diagonal del vector es: ");
-           	for (int f = 0; f <= FILAS_MATRIZ; f++){
+           	for (int f = 0; f < FILAS_MATRIZ; f++){
 				Console.WriteLine(diagonalVector[f]);
 			}
 		}
